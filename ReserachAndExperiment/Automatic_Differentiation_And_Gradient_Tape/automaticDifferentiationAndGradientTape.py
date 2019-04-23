1ea113a1cf840c3036417f4de05e7d28e7b120a1
Ivan Lam <>
2019-04-23 14:56:10 +0800

    commit

---
+++ ReserachAndExperiment/Automatic_Differentiation_And_Gradient_Tape/automaticDifferentiationAndGradientTape.py
@@ -0,0 +1,4 @@
+"""
+    In the previous tutorial we introduced Tensors and operations on them.
+    In this tutorial we will cover automatic differentiation, a key technique for optimizing machine learning models.
+"""
